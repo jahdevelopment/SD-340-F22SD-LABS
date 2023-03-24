c75b49315d4ff75b1b753d274232f1f0cc5439fe
juanhernandezdeveloper <jualherac@gmail.com>
2023-03-24 15:09:46 -0500

    Showing results with some little issue to resolve

---
+++ LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD_340_F22SD_LABS
+{
+    public class LinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public void AddData(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        
+        public void PrintData()
+        {
+            Node<T> current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+        }
+    }
+}
